62fd173554143e4c4c0c096e14715e34db851deb
Jonathan Doklovic <doklovic@atlassian.com>
2014-05-22 14:19:39 -0700

    now respecting core.eol

---
+++ jgit-flow-core/src/main/java/com/atlassian/jgitflow/core/CoreEol.java
@@ -0,0 +1,51 @@
+package com.atlassian.jgitflow.core;
+
+import org.eclipse.jgit.lib.Config;
+import org.eclipse.jgit.lib.ConfigConstants;
+import org.eclipse.jgit.lib.Config.ConfigEnum;
+
+public enum CoreEol implements ConfigEnum
+{
+    LF("lf", "\n"),
+    CRLF("crlf", "\r\n"),
+    NATIVE("native", System.getProperty("line.separator"));
+
+    public static CoreEol DEFAULT = NATIVE;
+    public static final String CONFIG_KEY_EOL = "eol";
+    
+    private final String configValue;
+    private final String eol;
+    
+    private CoreEol(final String configValue, final String eol)
+    {
+        this.configValue = configValue;
+        this.eol = eol;
+    }
+
+    public String getEol()
+    {
+        return eol;
+    }
+
+    public static CoreEol getConfigValue ( final Config gitConfig )
+    {
+        return
+                gitConfig.getEnum(CoreEol.values(),
+                        ConfigConstants.CONFIG_CORE_SECTION,
+                        null,
+                        CONFIG_KEY_EOL,
+                        DEFAULT);
+    }
+
+    @Override
+    public String toConfigValue ( )
+    {
+        return configValue;
+    }
+
+    @Override
+    public boolean matchConfigValue(final String in)
+    {
+        return configValue.equalsIgnoreCase(in);
+    }
+}
